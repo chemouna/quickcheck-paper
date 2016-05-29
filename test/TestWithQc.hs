1208338c7d7e8020da06e49dfa0e3aa05e96de31
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-05-29 05:17:33 +0200

    Add some examples.

--- test/TestWithQc.hs
+++ test/TestWithQc.hs
@@ -23,13 +23,30 @@ prop_Insert x xs = ordered xs ==> ordered (insert x xs)
 prop_Insert2 x xs = forAll orderedList $ \xs -> ordered (insert x xs)
     where types = x::Int
 
+-- Counting Trivial Cases
+-- <condition> `trivial` <property>
+-- prop_Insert3 x xs = ordered xs ==> null xs `trivial` ordered (insert x xs)
+--    where types = x::Int
+
 -- Classifying Test Cases
 -- classify <condition> <string>$ <property>
-prop_Insert3 x xs =
+prop_Insert4 x xs =
     ordered xs ==>
         classify (ordered (x:xs)) "at-head"$
         classify (ordered (xs++[x])) "at-tail"$
         ordered (insert x xs)
     where types = x::Int
 
+
+-- Collecting Data Values
+-- collect <expression>$ <property>
+prop_Insert5 x xs =
+    ordered xs ==> collect (length xs)$
+                   ordered (insert x xs)
+           where types = x::Int
+
+-- data generators
+-- choose :: Random a => (a, a) -> Gen a
+-- do i <- choose(0, length xs - 1)
+--    return (xs!!i)
 
